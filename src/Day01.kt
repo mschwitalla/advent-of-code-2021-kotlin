f2ec47b4a4332f5592db3e42fbb5200c470dec43
Martin Schwitalla <MSchwitalla1@gmx.de>
2021-12-02 22:48:56 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.mapIndexed { index, depth ->
+            if (index == 0) {
+                0
+            } else if (input[index - 1].toInt() < depth) {
+                1
+            } else {
+                0
+            }
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        for (i in 0..input.size - 4) {
+            val firstWindowSum = input.subList(i, i + 3).sumOf { it.toInt() }
+            val secondWindowSum = input.subList(i + 1, i + 4).sumOf { it.toInt() }
+
+            if (firstWindowSum < secondWindowSum) {
+                result++
+            }
+        }
+        return result
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
